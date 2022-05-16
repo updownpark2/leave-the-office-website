da50e12a48d1973e731dffc2f82a52dcc7f0369b
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2022-05-16 15:40:02 +0900

    leaveworkTime
    퇴근까지 남은 시간을 구현함
    //일시적인 시각이 아니라 지속되는 시간의 역할을 해야함 수정 필요

--- first.js
+++ first.js
@@ -1,28 +1,32 @@
 function clock(){
     const shorttime = new Date()
     const time = document.querySelector("h1 span")
-    time.innerText = `${String(shorttime.getHours()).padStart(2,"0")}:${String(shorttime.getMinutes()).padStart(2,"0")}:${String(shorttime.getSeconds()).padStart(2,"0")}`
+    time.innerText = `${String(shorttime.getHours()).padStart(2,"0")}:${String(shorttime.getMinutes()).padStart(2,"0")}`
 }
 clock()
 setInterval(clock,1000)
-
-const timetogoForm = document.querySelector(".timetogo_form")
-const timetogoValue1 = document.querySelector(".timetogo_input1")
-const timetogoValue2 = document.querySelector(".timetogo_input2")
-const h3 = document.querySelector("h3 span")
-function timecal(event){
+//현재시각
+const leaveForm = document.querySelector(".timetogo_form")
+const leaveInputH = document.querySelector(".timetogo_input1")
+const leaveInputM = document.querySelector(".timetogo_input2")
+function leaveclock(event){
     event.preventDefault();
-    const value1= timetogoValue1.value;
-    const value2=timetogoValue2.value;
-    timetogoValue1.value=""
-    timetogoValue2.value=""
-    localStorage.setItem("time",value1);
-    localStorage.setItem("min",value2);
-    const value1Save = localStorage.getItem("time")
-    const value2Save = localStorage.getItem("min")
-    const shorttime = new Date()
-  
-    h3.innerText = `${String(value1Save-shorttime.getHours()).padStart(2,"0")}:${String(value2Save-shorttime.getMinutes()).padStart(2,"0")}:${String(shorttime.getSeconds()).padStart(2,"0")}`
-timetogoForm.classList.add("hidden")
+    const leaveValue1 = leaveInputH.value;
+    const leaveValue2 = leaveInputM.value;
+    const h3 = document.querySelector("h3")
+    const h3span=document.querySelector("h3 span")
+    if(String(new Date().getHours())+String(new Date().getMinutes())>String(leaveValue1)+String(leaveValue2)){
+        h3.innerText="퇴근을 축하드립니다😋"
+    }
+    else{ if(leaveValue2-new Date().getMinutes()<0){
+        h3span.innerText=`${leaveValue1-new Date().getHours()-1}:${String((Math.abs(60-leaveValue2-new Date().getMinutes()))).padStart(2,"0")}`
+    }
+else{
+    h3span.innerText=`${leaveValue1-new Date().getHours()}:${String(leaveValue2-new Date().getMinutes()).padStart(2,"0")}`
 }
-timetogoForm.addEventListener("submit",timecal);
+    }
+leaveForm.classList.add("hidden")
+}
+leaveForm.addEventListener("submit",leaveclock);
+//남은시간 계산하는 시계까지만듦 그러나 지속되기위해서 Setinterval 함수필요
+//local에 저장 후에 로컬에 있는 수를 계속해서 호출하도록 해야한다.function()형태로
